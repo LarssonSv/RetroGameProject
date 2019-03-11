f936e8c4650cf25890189fb373b63e2eb9001e64
LarssonSv <Simon.Larsson1997@outlook.com>
2019-03-11 14:37:16 +0100

    Added objectPooling

---
+++ Assets/Scripts/BubbleProjectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleProjectile : MonoBehaviour
+{
+
+    [SerializeField] float speed = 200f;
+
+    //temp
+    void FixedUpdate()
+    {
+
+        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+    }
+
+}
