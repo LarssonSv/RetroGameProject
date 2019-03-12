281355285a405cb244acee5916043d4d00053059
charlottebarnekow <31241056+charlottebarnekow@users.noreply.github.com>
2019-03-12 11:56:35 +0100

    MenuManagement
    - added functions for buttons on PausMenu + controller input
    - added functions for buttons on GameOverMenu (no controller input)
    -

---
+++ Assets/Scripts/MenuManager/PausMenuManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausMenuManager: MonoBehaviour
+{
+
+    public GameObject PauseUI;
+  
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("P was pressed");
+            PauseUI.SetActive(true);
+            Pause();
+
+        }
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            Debug.Log("Joystick 0 was pressed");
+            PauseUI.SetActive(true);
+            Pause();
+        }
+    }
+
+     void Pause()
+    {
+     
+          Time.timeScale = 0;
+                    
+    }
+
+
+    public void Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+
+    {
+        Debug.Log("Restart Clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("App Quit");
+        Application.Quit();
+    }
+
+
+}
