9ffd9f201235b4f54d8e8662b7a09255dbd3bb93
Anton Samuelsson <antonra.samuelsson@gmail.com>
2019-03-12 11:50:15 +0100

    - Tuned Boat movement
    Goes faster in when going back and slower when pushing forward

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
-    public float speed = 5f;
-
+    public float speed = 6f;
+    public bool speedModifier = false;
     private Rigidbody2D rb;
+    float moveHorizontal;
+    float moveVertical;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            speedModifier = true;
+            speed = 2.5f;
+        }
+        else
+        {
+            speedModifier = false;
+            speed = 6f;
+        }
+    }
+
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        rb.AddRelativeForce(movement * speed); 
+        rb.AddRelativeForce(movement * speed);
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
     }
+
+
+
 }
