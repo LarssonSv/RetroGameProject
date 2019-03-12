90e9cc6e1ba3456f46ddaad8a4fcba92f253efc4
LarssonSv <Simon.Larsson1997@outlook.com>
2019-03-12 15:01:00 +0100

    Revamped Obstacle spawner

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,28 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 //Author: Simon
 public class ObstacleSpawner : MonoBehaviour
 {
-    //Publics
     [Header("Spawn Behavior")]
     [Range(0.0f, 100.0f)][Tooltip("Chance of to actually spawn, will properly be moved to be handle by the prefab spawned")] public float spawnChance = 5.0f;
     [Range(0.0f, 15.0f)][Tooltip("Delay before spawn start")]public float delay = 5.0f;
     [Range(0.0f, 10.0f)][Tooltip("Delay before spawn start")]public float repeatRate = 5.0f;
 
-    //Privates
     [Header("Setup")]
     [SerializeField] List<GameObject> obstacles = new List<GameObject>();
-    [SerializeField] RailDriver railDriver;
+    [SerializeField] Tilemap map;
+    [SerializeField] TileBase temp; //TODO: Will be moved to list later
+    GameObject currentCamera;
 
     [Header("Spawn Position Offset from center of screen")]
-    [Range(0.0f, 50.0f)] [Tooltip("Spawn offset on X-axis")] public float offsetX = 5.0f;
-    [Range(0.0f, 50.0f)] [Tooltip("Spawn offset on Y-axis")] public float offsetY = 5.0f;
+    [Range(0.0f, 50.0f)] [Tooltip("Spawn offset on X-axis")] public int offsetX = 5;
 
 
     private void Start()
     {
-        if(obstacles.Count > 0)
+        currentCamera = GameManager.GM.CurrentGameMode.currentCamera;
+        if(temp)
         {
             InvokeRepeating("AttemptSpawn", delay, repeatRate);
         }
@@ -33,15 +34,33 @@ public class ObstacleSpawner : MonoBehaviour
     {
         if(Random.Range(0.0f, 100.0f) < spawnChance)
         {
-            Instantiate(obstacles[Random.Range(-1, obstacles.Count + 1)], railDriver.CurrentRailPos, new Quaternion(0f, 0f, 0f, 0f));
-            return true;
+            Vector3Int spawnPos = Vector3Int.FloorToInt((currentCamera.transform.position + new Vector3(offsetX, 0, 0)));
+            int i = 0;
+
+            while(i < 10)
+            {
+                if(!GetCell(map,map.GetCellCenterWorld(spawnPos)))
+                {
+                    spawnPos.y -= 1;
+                    i++;
+                }
+                else
+                {
+                    spawnPos.y += 1;
+                    map.SetTile(spawnPos, temp);
+                    return true;
+                }
+
+            }
         }
 
         return false;
     }
-
-
-
+    
+    private TileBase GetCell(Tilemap tilemap, Vector3 cellWorldPos)
+    {
+        return tilemap.GetTile(tilemap.WorldToCell(cellWorldPos));
+    }
 
 
 }
