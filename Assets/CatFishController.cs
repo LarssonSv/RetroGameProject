4923e801e419ade4db7134a43d01eeda2fdabe44
charlottebarnekow <31241056+charlottebarnekow@users.noreply.github.com>
2019-03-13 13:38:57 +0100

    - added player controller for fish, added script bubble fire

---
+++ Assets/CatFishController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatFishController : MonoBehaviour
+{
+    public float MoveSpeed;
+  
+    private Rigidbody2D rb2D;
+    float moveHorizontal;
+    float moveVertical;
+
+    public GameObject bubble;
+    public Transform bubbleSpawner;
+    public float bubbleRate;
+    public float bubbleSpeed;
+
+  
+    private float nextBubble;
+
+    public float bubbleForce = 750.0f;
+
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    void Update()
+    {
+       
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2D.AddRelativeForce(movement * MoveSpeed);
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextBubble)
+        {
+            nextBubble = Time.time + bubbleRate;
+            Instantiate(bubble, bubbleSpawner.position, bubbleSpawner.rotation);
+            GetComponent<Rigidbody>().AddForce(transform.right * bubbleForce);
+
+        }
+     
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/BubbleFire.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleFire : MonoBehaviour
+{
+
+    public float bubbleForce = 750.0f;
+
+    void onTriggerEnter2D (Collider2D target)
+    {
+        if (target.gameObject.tag == "FirePoint") GetComponent<Rigidbody>().AddForce(transform.right * bubbleForce);
+    }
+}
