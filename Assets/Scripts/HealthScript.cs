4a49fbae2349f79778489dc3886cfdd809397c46
Anton Samuelsson <antonra.samuelsson@gmail.com>
2019-03-14 15:56:38 +0100

    Health Scriipt added (wip) and added boat and boat collider to main scene

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public int startingHealth = 3;
+    public int currentHealth;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    BoatPlayerController boatMovement;
+
+    private bool isDead;
+    private bool damaged;
+
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+        boatMovement = GetComponent<BoatPlayerController>();
+    }
+
+
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        boatMovement.enabled = false;
+    }
+
+
+        
+}
