9e159068c51f71f107100da30751f4290ecaf8fd
LarssonSv <Simon.Larsson1997@outlook.com>
2019-03-21 13:33:51 +0100

    Added mine handling

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+
+    GameObject fish;
+
+
+    private void Start()
+    {
+        fish = GameManager.GM.CurrentGameMode.currentFishPlayer;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.gameObject == fish )
+        {
+            fish.GetComponent<HealthScript>().TakeDamage(damage);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+
+}
